af49f41b61cc8ae6681a9f86a7534373fc9c926f
Josh Fang <joshuafang13@gmail.com>
2020-09-06 19:14:01 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ const session = require('express-session')
 const usePassport = require('./config/passport')
 const flash = require('connect-flash')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const routes = require('./routes')
 
+const PORT = process.env.PORT
+
 app = express()
-const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
